a4fced17e087453abe3805384b1a79e1b8e4b30f
Vita Fomin <vita.bukhalo@gmail.com>
2023-07-17 00:27:44 -0400

    cleaned up code in contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-
 function Contact() {
-    return (
-        <section id="contact">
-            <div className="contact-title">
-            <h2>Contact Me</h2>
-            </div>
-            <div id="mycon">
-            <a href="tel:700-123-4568">303-875-3607</a>
-            <a href="mailto:vita.bukhalo@gmail.com">vita.bukhlalo@gmail.com</a>
-           <a href="https://github.com/vitafomin">GitHub</a>
-            </div>
-        </section>
-    )
-};
+  return (
+    <section id="contact">
+      <div className="contact-title">
+        <h2>Contact Me</h2>
+      </div>
+      <div id="mycon">
+        <a href="tel:700-123-4568">303-875-3607</a>
+        <a href="mailto:vita.bukhalo@gmail.com">vita.bukhlalo@gmail.com</a>
+        <a href="https://github.com/vitafomin">GitHub</a>
+      </div>
+    </section>
+  );
+}
 
 export default Contact;
