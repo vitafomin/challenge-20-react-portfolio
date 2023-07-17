6e5349593f5c7546146fed8a9a4581143a4366aa
Vita Fomin <vita.bukhalo@gmail.com>
2023-07-17 00:28:22 -0400

    cleaned up code in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,30 @@
 import React from "react";
-import Resume20 from "../images/Resume20.pdf"
+import Resume20 from "../images/Resume20.pdf";
 
 function Header() {
-    return (
+  return (
     <header className="head">
-        <h1 id="name">Vita Fomin</h1>
-        <nav>
-            <ul>
-                <li>
-                    <a href="#about">About Me</a>
-                </li>
-                <li>
-                    <a href="#work">Work</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact Me</a>
-                </li>
-                <li>
-                    <a href="https://docs.google.com/document/d/1jtRzZWAGF81lA6T1tq5ylLSOFa1es8mveDIBXrbI6Vo/edit?usp=sharing">Resume</a>
-                </li>
-            </ul>
-        </nav>
+      <h1 id="name">Vita Fomin</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#work">Work</a>
+          </li>
+          <li>
+            <a href="#contact">Contact Me</a>
+          </li>
+          <li>
+            <a href="https://docs.google.com/document/d/1jtRzZWAGF81lA6T1tq5ylLSOFa1es8mveDIBXrbI6Vo/edit?usp=sharing">
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
-    );
-};
+  );
+}
 
 export default Header;
